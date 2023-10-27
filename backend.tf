c070c3f2315dde968156ac4041dbe429419bdc2f
shrikantalone66 <149130079+shrikantalone66@users.noreply.github.com>
2023-10-26 19:30:14 -0800

    Add files via upload

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "mydev-tf-state-bucket-1666"
+    key = "main"
+    region = "ap-south-1" 
+    #region = "us-east-1"
+    dynamodb_table = "my-dynamodb-table"
+  }
+}
