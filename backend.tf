5b9a2b1da4ccfedcfc8e891ad313606a338697bf
shrikantalone66 <149130079+shrikantalone66@users.noreply.github.com>
2024-05-18 12:05:29 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
   bucket = "shrikant-bucketss"
     key = "main"
-    region = "ap-south-1" 
-    #region = "us-east-1"
+   # region = "ap-south-1" 
+    region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
